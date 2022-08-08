f94afeb3c48d04c23291cf1f4fb5388d176bc79f
shahriararefin <shahriararefin7@gmail.com>
2022-08-09 04:51:49 +0600

    First Push

---
+++ 1.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+
+    int a=4,b=5,c=53;
+
+    if(a<b && b>c)
+        printf("B is largest");
+        
+        else if(a>c)
+
+            printf("A is largest");
+
+            else
+                printf("C is largest");
+        
+    
+
+    return 0;
+}
